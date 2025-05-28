e05474b76f2f5c0370263f94c9b1fdab11955ac7
Ravi Correia <ravic@ciandt.com>
2025-05-28 12:56:51 -0300

    Add: Monokai-pro theme
    Added Monokai-Pro theme to neovim.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
             "LazyVim/LazyVim",
             import = "lazyvim.plugins",
             opts = {
-                colorscheme = "gruvbox-material",
+                colorscheme = "monokai-pro",
             },
         },
         -- import/override with your plugins
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,234 +1,235 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
-    event = "VeryLazy",
-    priority = 1000,
-    opts = {},
-    config = function()
-      local tokyo = require("tokyonight")
-      tokyo.setup({
-        style = "night",
-        transparent = true,
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        },
-      })
-    end,
-  },
-  {
-    "rose-pine/neovim",
-    -- enable = false,
-    name = "rose-pine",
-    event = "VeryLazy",
-    priority = 250,
-    config = function()
-      local rose_pine = require("rose-pine")
-      rose_pine.setup({
-        opt = true,
-        variant = "auto", -- auto, main, moon, or dawn
-        dark_variant = "main", -- main, moon, or dawn
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
-
-        enable = {
-          terminal = true,
-          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true, -- Handle deprecated options automatically
-        },
-
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = true,
-        },
-
-        groups = {
-          border = "muted",
-          link = "iris",
-          panel = "surface",
-
-          error = "love",
-          hint = "iris",
-          info = "foam",
-          note = "pine",
-          todo = "rose",
-          warn = "gold",
-
-          git_add = "foam",
-          git_change = "rose",
-          git_delete = "love",
-          git_dirty = "rose",
-          git_ignore = "muted",
-          git_merge = "iris",
-          git_rename = "pine",
-          git_stage = "iris",
-          git_text = "rose",
-          git_untracked = "subtle",
-
-          h1 = "iris",
-          h2 = "foam",
-          h3 = "rose",
-          h4 = "gold",
-          h5 = "pine",
-          h6 = "foam",
-        },
-
-        palette = {
-          -- Override the builtin palette per variant
-          main = {
-            -- gold = "#f2e9e1",
-          },
-        },
-
-        highlight_groups = {
-          -- Comment = { fg = "foam" },
-          -- VertSplit = { fg = "muted", bg = "muted" },
-        },
-
-        before_highlight = function(group, highlight, palette)
-          -- Disable all undercurls
-          -- if highlight.undercurl then
-          --     highlight.undercurl = false
-          -- end
-          --
-          -- Change palette colour
-          -- if highlight.fg == palette.pine then
-          --     highlight.fg = palette.foam
-          -- end
+    {
+        "folke/tokyonight.nvim",
+        name = "tokyonight",
+        event = "VeryLazy",
+        priority = 1000,
+        opts = {},
+        config = function()
+            local tokyo = require("tokyonight")
+            tokyo.setup({
+                style = "night",
+                transparent = true,
+                styles = {
+                    sidebars = "transparent",
+                    floats = "transparent",
+                },
+            })
         end,
-      })
+    },
+    {
+        "rose-pine/neovim",
+        -- enable = false,
+        name = "rose-pine",
+        event = "VeryLazy",
+        priority = 250,
+        config = function()
+            local rose_pine = require("rose-pine")
+            rose_pine.setup({
+                opt = true,
+                variant = "auto", -- auto, main, moon, or dawn
+                dark_variant = "main", -- main, moon, or dawn
+                dim_inactive_windows = false,
+                extend_background_behind_borders = true,
 
-      -- vim.cmd("colorscheme rose-pine")
-      -- vim.cmd("colorscheme rose-pine-main")
-      -- vim.cmd("colorscheme rose-pine-moon")
-      -- vim.cmd("colorscheme rose-pine-dawn")
-    end,
-  },
+                enable = {
+                    terminal = true,
+                    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+                    migrations = true, -- Handle deprecated options automatically
+                },
 
-  {
-    "craftzdog/solarized-osaka.nvim",
-    -- enable = false,
-    -- priority = 1000,
-    event = "VeryLazy",
-    opts = { transparent = true },
-    config = function()
-      local osaka = require("solarized-osaka")
-      --- @diagnostic disable-next-line: missing-fields
-      osaka.setup({
-        opt = true,
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        transparent = true, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "transparent", -- style for floating windows
-        },
-        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false, -- dims inactive windows
-        lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+                styles = {
+                    bold = true,
+                    italic = true,
+                    transparency = true,
+                },
 
-        --- You can override specific color groups to use other groups or a hex color
-        --- function will be called with a ColorScheme table
-        -- @param colors ColorScheme
-        on_colors = function(colors)
-          colors.bg_statusline = "transparent"
+                groups = {
+                    border = "muted",
+                    link = "iris",
+                    panel = "surface",
+
+                    error = "love",
+                    hint = "iris",
+                    info = "foam",
+                    note = "pine",
+                    todo = "rose",
+                    warn = "gold",
+
+                    git_add = "foam",
+                    git_change = "rose",
+                    git_delete = "love",
+                    git_dirty = "rose",
+                    git_ignore = "muted",
+                    git_merge = "iris",
+                    git_rename = "pine",
+                    git_stage = "iris",
+                    git_text = "rose",
+                    git_untracked = "subtle",
+
+                    h1 = "iris",
+                    h2 = "foam",
+                    h3 = "rose",
+                    h4 = "gold",
+                    h5 = "pine",
+                    h6 = "foam",
+                },
+
+                palette = {
+                    -- Override the builtin palette per variant
+                    main = {
+                        -- gold = "#f2e9e1",
+                    },
+                },
+
+                highlight_groups = {
+                    -- Comment = { fg = "foam" },
+                    -- VertSplit = { fg = "muted", bg = "muted" },
+                },
+
+                before_highlight = function(group, highlight, palette)
+                    -- Disable all undercurls
+                    -- if highlight.undercurl then
+                    --     highlight.undercurl = false
+                    -- end
+                    --
+                    -- Change palette colour
+                    -- if highlight.fg == palette.pine then
+                    --     highlight.fg = palette.foam
+                    -- end
+                end,
+            })
+
+            -- vim.cmd("colorscheme rose-pine")
+            -- vim.cmd("colorscheme rose-pine-main")
+            -- vim.cmd("colorscheme rose-pine-moon")
+            -- vim.cmd("colorscheme rose-pine-dawn")
         end,
+    },
 
-        --- You can override specific highlights to use other groups or a hex color
-        --- function will be called with a Highlights and ColorScheme table
-        -- @param highlights Highlights
-        -- @param colors ColorScheme
-        on_highlights = function(highlights, colors) end,
-      })
-    end,
-  },
+    {
+        "craftzdog/solarized-osaka.nvim",
+        -- enable = false,
+        -- priority = 1000,
+        event = "VeryLazy",
+        opts = { transparent = true },
+        config = function()
+            local osaka = require("solarized-osaka")
+            --- @diagnostic disable-next-line: missing-fields
+            osaka.setup({
+                opt = true,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                transparent = true, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = true },
+                    keywords = { italic = true },
+                    functions = {},
+                    variables = {},
+                    -- Background styles. Can be "dark", "transparent" or "normal"
 
-  {
-    "sainnhe/gruvbox-material",
-    -- enable = false,
-    event = "VeryLazy",
-    config = function()
-      vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_transparent_background = 2
-    end,
-  },
+                    floats = "transparent", -- style for floating windows
+                },
+                sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+                day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+                hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+                dim_inactive = false, -- dims inactive windows
+                lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 
-  {
-    "rebelot/kanagawa.nvim",
-    -- enable = false,
-    -- priority = 550,
-    event = "VeryLazy",
-    config = function()
-      local kanagawa = require("kanagawa")
-      kanagawa.setup({
-        compile = false, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = true, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = { bg_gutter = "none" } },
-        },
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            FloatTitle = { bg = "none" },
+                --- You can override specific color groups to use other groups or a hex color
+                --- function will be called with a ColorScheme table
+                -- @param colors ColorScheme
+                on_colors = function(colors)
+                    colors.bg_statusline = "transparent"
+                end,
 
-            -- Save an hlgroup with dark background and dimmed foreground
-            -- so that you can use it where your still want darker windows.
-            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-            -- Popular plugins that open floats will link to NormalFloat by default;
-            -- set their background accordingly if you wish to keep them dark and borderless
-            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-            -- More uniform colors for the popup menu
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-
-            -- Block-like modern Telescope UI
-            TelescopeTitle = { fg = theme.ui.special, bold = true },
-            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-          }
+                --- You can override specific highlights to use other groups or a hex color
+                --- function will be called with a Highlights and ColorScheme table
+                -- @param highlights Highlights
+                -- @param colors ColorScheme
+                on_highlights = function(highlights, colors) end,
+            })
         end,
-        theme = "dragon", -- Load "wave" theme when 'background' option is not set
-        background = { -- map the value of 'background' option to a theme
-          dark = "dragon", -- try "dragon" !
-          light = "lotus",
-        },
-      })
+    },
 
-      -- setup must be called before loading
-      -- vim.cmd("colorscheme kanagawa")
-    end,
-  },
+    {
+        "sainnhe/gruvbox-material",
+        -- enable = false,
+        event = "VeryLazy",
+        config = function()
+            vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_transparent_background = 2
+        end,
+    },
+
+    {
+        "loctvl842/monokai-pro.nvim",
+        name = "monokai-pro",
+        event = "VeryLazy",
+        priority = 1000,
+        opts = {},
+        config = function()
+            local monokai = require("monokai-pro")
+            monokai.setup({
+                transparent_background = false,
+                terminal_colors = true,
+                devicons = true, -- highlight the icons of `nvim-web-devicons`
+                styles = {
+                    comment = { italic = true },
+                    keyword = { italic = true }, -- any other keyword
+                    type = { italic = true }, -- (preferred) int, long, char, etc
+                    storageclass = { italic = true }, -- static, register, volatile, etc
+                    structure = { italic = true }, -- struct, union, enum, etc
+                    parameter = { italic = true }, -- parameter pass in function
+                    annotation = { italic = true },
+                    tag_attribute = { italic = true }, -- attribute of tag in reactjs
+                },
+                filter = "ristretto", -- classic | octagon | pro | machine | ristretto | spectrum
+                -- Enable this will disable filter option
+                day_night = {
+                    enable = false, -- turn off by default
+                    day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+                    night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+                },
+                inc_search = "background", -- underline | background
+                background_clear = {
+                    -- "float_win",
+                    "toggleterm",
+                    "telescope",
+                    -- "which-key",
+                    "renamer",
+                    "notify",
+                    -- "nvim-tree",
+                    -- "neo-tree",
+                    -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+                }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+                plugins = {
+                    bufferline = {
+                        underline_selected = false,
+                        underline_visible = false,
+                    },
+                    indent_blankline = {
+                        context_highlight = "default", -- default | pro
+                        context_start_underline = false,
+                    },
+                },
+                ------@param c Colorscheme
+                ---override = function(c) end,
+                ------@param cs Colorscheme
+                ------@param p ColorschemeOptions
+                ------@param Config MonokaiProOptions
+                ------@param hp Helper
+                ---override = function(
+                ---    cs: Colorscheme,
+                ---    p: ColorschemeOptions,
+                ---    Config: MonokaiProOptions,
+                ---    hp: Helper
+                ---)
+                ---end,
+            })
+        end,
+    },
 }
