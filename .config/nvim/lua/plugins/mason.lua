86886725319292a29efa3bbbf0bdd0f39156a75b
Ravi Xavier <ravic@ciandt.com>
2025-08-08 19:08:46 -0300

    Edit: minor changes to nvim

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,9 +1,8 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "jdtls",
-      "lemminx",
+    "williamboman/mason.nvim",
+    opts = {
+        ensure_installed = {
+            "jdtls",
+        },
     },
-  },
 }
